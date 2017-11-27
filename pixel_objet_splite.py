94b08a5df2c680f1efbf483c6ff6335342cd3c35
Jérémy Bressand <j.bressand.34@gmail.com>
2017-11-27 17:14:07 +0100

    construction des données des objets et vérification intégrité des données par affichage

---
+++ pixel_objet_splite.py
@@ -0,0 +1,130 @@
+# encoding : utf-8
+import json
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
+from operator import itemgetter
+import operator
+import time
+
+"""
+Permets de construire les fichiers :
+pixel_objet_leve_splite.json
+pixel_objet_non_leve_splite.json
+"""
+
+file_objet = open("../data_4_tp/data_segmentation_objet.json", "r")
+objets_pixels = json.load(file_objet)
+file_objet.close()
+
+file = open("../data_4_tp/data_parcelle.json", "r")
+parcelles_pixels = json.load(file)
+file.close()
+
+file = open("../data_4_tp/parcelles_levees.json", "r")
+parcelles_dates_levees = json.load(file)
+file.close()
+
+"""
+On determine quels sont les pixels leves et non leves dans les parcelles
+"""
+
+dates = ["20160806", "20161003", "20161102", "20161122","20161202"]
+
+pixels_parcelles_leves = [] #couple (date,pixel)
+pixels_parcelles_non_leves = [] #couple (date,pixel)
+
+array_pixel = np.array([p for k,t in parcelles_pixels.items() for p in t])
+l_min = np.min(array_pixel[:,0])
+l_max = np.max(array_pixel[:,0])
+c_min = np.min(array_pixel[:,1])
+c_max = np.max(array_pixel[:,1])
+"""
+print(l_min)
+print(l_max)
+print(c_min)
+print(c_max)
+"""
+
+for nl in range(l_min,l_max+1):
+	for nc in range(c_min,c_max+1):
+		pixel = [nl,nc]
+		exist = False
+		for date in dates:
+			levee = False
+			non_levee = False
+			for parcelle, date_parcelle in parcelles_dates_levees.items():
+				if pixel in parcelles_pixels[parcelle]:
+					if int(date) >= int(date_parcelle):
+						levee = True
+					else:
+						non_levee = True
+			if levee and non_levee :
+				raise Exception()
+			elif levee:
+				pixels_parcelles_leves.append((date,pixel))
+			elif non_levee:
+				pixels_parcelles_non_leves.append((date,pixel))
+
+print("Nombre de pixels leves dans les parcelles : "+str(len(pixels_parcelles_leves)))	
+print("Nombre de pixels non leves dans les parcelles : "+str(len(pixels_parcelles_non_leves)))				
+
+communs = []
+for tab in pixels_parcelles_non_leves:
+	if tab in pixels_parcelles_leves:
+		communs.append(tab)
+
+print(communs)
+
+"""
+On determine quels sont les objets leves et non leves
+On leur enleve les pixels ne correspondant pas à leur etat
+"""
+support = 2/3 
+#(nbPixelLeves/(nbPixelLeves+nbPixelNonLeves)>support -> objet leve
+#(nbPixelLeves/(nbPixelLeves+nbPixelNonLeves)<(1-support) et >0 -> objet non leve
+
+objets_leves = {}
+objets_non_leves = {}
+
+for date, objs in objets_pixels.items():
+	for obj, pixels in objs.items():
+		liste_pixel_leves = []
+		liste_pixel_non_leves = []
+		liste_pixel_unknown = []
+		for pixel in pixels:
+			if (date,pixel) in pixels_parcelles_leves:
+				liste_pixel_leves.append(pixel)
+			elif (date,pixel) in pixels_parcelles_non_leves:
+				liste_pixel_non_leves.append(pixel)
+			else:
+				liste_pixel_unknown.append(pixel)
+		nbl = len(liste_pixel_leves)
+		nbnl = len(liste_pixel_non_leves)
+		if nbl != 0 or nbnl != 0:
+			test = nbl/(nbl+nbnl)
+			if test >= support:
+				liste_pixels = liste_pixel_leves + liste_pixel_unknown
+				objets_leves[date+" "+obj] = pd.Series(liste_pixels).to_json(orient='values')
+			elif (1-test) >= support:
+				liste_pixels = liste_pixel_non_leves + liste_pixel_unknown
+				objets_non_leves[date+" "+obj] = pd.Series(liste_pixels).to_json(orient='values')
+
+print("Nombre d'objet splite leves : "+str(len(list(objets_leves.keys()))))	
+print("Nombre d'objet splite non leves : "+str(len(list(objets_non_leves.keys()))))
+
+pixels_leves = [val for obj, t in objets_leves.items() for val in json.loads(t)]
+pixels_non_leves = [val for obj, t in objets_non_leves.items() for val in json.loads(t)]
+
+print("Nombre final de pixels leves : "+str(len(pixels_leves)))	
+print("Nombre final de pixels non leves : "+str(len(pixels_non_leves)))
+
+
+file = open("../data_4_tp/pixel_objet_leve_splite.json", "w")
+json.dump(objets_leves,file)
+file.close()
+
+file = open("../data_4_tp/pixel_objet_non_leve_splite.json", "w")
+json.dump(objets_non_leves,file)
+file.close()
